21086b8505eac7f5455448714e7ab62b0e71d621
Tristan (t4t5) <t4t5@hey.com>
2026-07-19 07:03:17 +0100

    Fix maximum pagesize error

--- src/constants.rs
+++ src/constants.rs
@@ -3,4 +3,14 @@ pub const DEFAULT_BASE_URL: &str = "https://mail.proton.me/api";
 pub const APP_VERSION: &str = "Other";
 pub const USER_AGENT: &str = concat!("caldir-provider-proton/", env!("CARGO_PKG_VERSION"));
 pub const ITEM_UID_PROPERTY: &str = "X-PROTON-ITEM";
-pub const PAGE_SIZE: usize = 150;
+pub const PAGE_SIZE: usize = 100;
+
+#[cfg(test)]
+mod tests {
+    use super::PAGE_SIZE;
+
+    #[test]
+    fn proton_page_size_is_within_api_bounds() {
+        assert!((1..=100).contains(&PAGE_SIZE));
+    }
+}
